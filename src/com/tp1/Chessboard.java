cc970994b9276674de014d93b2f41db3ac90486b
Ingrid <ingridespangler@gmail.com>
2018-09-28 10:00:23 -0300

    Updating dir tree

---
+++ src/com/tp1/Chessboard.java
@@ -0,0 +1,29 @@
+package com.tp1;
+
+import java.awt.*;
+import javax.swing.*;
+public class Chessboard extends JPanel {
+
+    public int[][] BoardMatrix = new int[8][8];
+
+    public void paintComponent( Graphics g ){
+        boolean sem = false;
+
+        super.paintComponent( g );
+        this.setBackground(Color.LIGHT_GRAY);
+        System.out.println("AAAAA");
+        for(int i = 0; i < 8; i += 1){
+            for(int j = 0; j < 8; j += 1){
+                if(sem)
+                    g.fillRect(i*80, j*80, 80, 80);
+                sem = !sem;
+                JLabel jlabel = new JLabel(String.valueOf(this.BoardMatrix[i][j]));
+                jlabel.setFont(new Font("Verdana",1,30));
+                jlabel.setForeground(Color.RED);
+                jlabel.setBounds((i*80)+20, (j*40), i*80 + 80, j*80 + 80);
+                this.add(jlabel);
+            }
+            sem = !sem;
+        }
+    }
+}
---
+++ src/com/tp1/Horse.java
@@ -0,0 +1,89 @@
+package com.tp1;
+
+
+import java.util.Random;
+
+public class Horse {
+
+    private int[] pos;
+    public int[][] LogMatrix;
+
+    private int[] choose_initial_position() {
+        Random r = new Random();
+        int s = r.nextInt(64);
+        int row = s/8;
+        int column = (s % 8);
+        int[] pos = new int[2];
+        pos[0] = row;
+        pos[1] = column;
+
+        this.pos = pos;
+
+        return pos;
+    }
+
+    private int[][] get_possible_next_moves(int[] pos){
+        //the next moves will always be like "column/row +- 1, +- 2"
+        int[][] moves = {{pos[0]+1, pos[1]+2},
+                {pos[0]+1, pos[1]-2},
+                {pos[0]-1, pos[1]+2},
+                {pos[0]-1, pos[1]-2},
+                {pos[0]+2, pos[1]+1},
+                {pos[0]+2, pos[1]-1},
+                {pos[0]-2, pos[1]+1},
+                {pos[0]-2, pos[1]-1}};
+
+        return moves;
+    }
+
+    private boolean already_visited(int[] pos){
+        if (this.LogMatrix[pos[0]][pos[1]] != 0){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean is_valid(int[] pos){
+
+        if (pos[0] >= 8 || pos[0] < 0 || pos[1] >= 8 || pos[1] < 0){
+            return false;
+        }
+        return true;
+    }
+
+    //main recursive method, gets a position and fills the log matrix
+    private boolean visit(int[] pos, int step){
+
+        int[][] candidates;
+        int i = 0;
+        boolean completed_way = false;
+
+        //take the step
+        this.LogMatrix[pos[0]][pos[1]] = step;
+        //if the horse completed the board
+        if(step == 64)
+            return true;
+
+        candidates = this.get_possible_next_moves(pos);
+        while(i < 8) {
+            if (this.is_valid(candidates[i]) && !this.already_visited(candidates[i]))
+                completed_way = this.visit(candidates[i], step + 1);
+                if(completed_way)
+                    return true;
+            i++;
+        }
+        this.LogMatrix[pos[0]][pos[1]] = 0;
+        return false;
+    }
+
+    //public caller function
+    public int[][] run(){
+        int[] starting_point = this.choose_initial_position();
+        int i, j;
+        this.LogMatrix = new int[8][8];
+        boolean result;
+        result = this.visit(pos, 1);
+        return LogMatrix;
+    }
+
+}
